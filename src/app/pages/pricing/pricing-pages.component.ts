bb644ce83a0f421411c85fe40086d3afbb9eeb3b
Nick Paz <31939425+nickpazfernande@users.noreply.github.com>
2025-09-18 23:01:42 -0300

    Agregar soporte para @netlify/angular-runtime y mejorar la navegación

--- src/app/pages/pricing/pricing-pages.component.ts
+++ src/app/pages/pricing/pricing-pages.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { isPlatformServer } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
@@ -11,8 +12,20 @@ import { Meta, Title } from '@angular/platform-browser';
 export default class PricingPagesComponent implements OnInit {
   private title = inject(Title);
   private meta = inject(Meta);
+  private platform = inject(PLATFORM_ID);
 
   ngOnInit() {
+    //Esto se ejecuta en el servidor y en el cliente
+    //El server muestra "PLATFORM_ID: server"
+    //El cliente muestra "PLATFORM_ID: browser"
+    console.log('PLATFORM_ID:', this.platform);
+    //Este codigo solo funciona del lado del cliente
+    // if (!isPlatformServer(this.platform)) {
+    //Esto no sirve para SEO
+    //El HTML ya fue generado en el servidor
+    //y el cliente solo lo "hidrata"
+    //   document.title = 'Pricing Plans - Nick';
+    // }
     this.title.setTitle('Pricing Plans');
     this.meta.updateTag({ name: 'description', content: 'Learn more about our pricing plans and options.' });
     this.meta.updateTag({ name: 'og:title', content: 'Pricing Plans' });
