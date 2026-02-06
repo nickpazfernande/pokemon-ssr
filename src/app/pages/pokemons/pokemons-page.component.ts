8b468a6cbac0a7dc2462b513f82b7a695634ff03
Nick Paz <31939425+nickpazfernande@users.noreply.github.com>
2026-02-06 08:29:33 -0300

    Agregar manejo de ciclo de vida en PokemonsPageComponent

--- src/app/pages/pokemons/pokemons-page.component.ts
+++ src/app/pages/pokemons/pokemons-page.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, OnInit, signal } from '@angular/core';
+import { ApplicationRef, ChangeDetectionStrategy, Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
 import { PokemonsListComponent } from "../../pokemons/components/pokemons-list/pokemons-list.component";
 import { PokemonListSkeletonComponent } from "../../pokemons/ui/pokemon-list-skeleton/pokemon-list-skeleton.component";
 
@@ -8,12 +8,23 @@ import { PokemonListSkeletonComponent } from "../../pokemons/ui/pokemon-list-ske
   templateUrl: './pokemons-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class PokemonsPageComponent implements OnInit {
+export default class PokemonsPageComponent implements OnDestroy, OnInit {
+
   public isLoading = signal(true);
+
+  private appRef = inject(ApplicationRef);
+
+  private $appState = this.appRef.isStable.subscribe((isStable) => {
+    console.log({ isStable })
+  });
 
   ngOnInit() {
     setTimeout(() => {
       this.isLoading.set(false);
     }, 1500);
+  }
+
+  ngOnDestroy(): void {
+    this.$appState.unsubscribe();
   }
 }
