1103fd8fa5db48b08ff9cc6e0657b4b7b7b02a87
Nick Paz <31939425+nickpazfernande@users.noreply.github.com>
2026-02-13 10:15:51 -0300

    Agregar manejo de metadatos y título en la página del Pokémon

--- src/app/pages/pokemon-page.component/pokemon-page.component.ts
+++ src/app/pages/pokemon-page.component/pokemon-page.component.ts
@@ -2,6 +2,8 @@ import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@ang
 import { Pokemon } from '../../pokemons/interfaces/pokemon.interface';
 import { PokemonsServices } from '../../pokemons/services/pokemons.services';
 import { ActivatedRoute } from '@angular/router';
+import { tap } from 'rxjs';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-pokemon-page',
@@ -14,11 +16,23 @@ export default class PokemonPageComponent implements OnInit {
   private pokemonsServices = inject(PokemonsServices);
   private route = inject(ActivatedRoute);
   public pokemon = signal<Pokemon | null>(null);
+  private title = inject(Title);
+  private meta = inject(Meta);
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id') || '';
-    this.pokemonsServices.loadPokemon(id).subscribe(pokemon => {
-      this.pokemon.set(pokemon);
-    });
+    this.pokemonsServices.loadPokemon(id)
+      .pipe(
+        tap(({ name, id }) => {
+          this.title.setTitle(`${name} #${id} | Pokédex App`);
+          this.meta.updateTag({ name: 'description', content: `Details and information about ${name}, Pokémon #${id}` });
+          this.meta.updateTag({ name: 'og:title', content: `${name} #${id} | Pokédex App` });
+          this.meta.updateTag({ name: 'og:description', content: `Details and information about ${name}, Pokémon #${id}` });
+          this.meta.updateTag({ name: 'og:image', content: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png` });
+        })
+      )
+      .subscribe(pokemon => {
+        this.pokemon.set(pokemon);
+      });
   }
 }
